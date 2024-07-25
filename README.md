d5a6ce0f1908971bf82d066642e87b47d08d88c2
Nikolay Bayborodin <nbbrdn@gmail.com>
2024-07-25 23:31:38 +1000

    Init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Go Genie
+Go game engine and Go game AI using deep learnig technics.
---
+++ goboard_slow.py
@@ -0,0 +1,148 @@
+import copy
+from gotypes import Player
+
+class Move():
+    def __init__(self, point=None, is_pass=False, is_resign=False):
+        assert(point is not None) ^ is_pass ^ is_resign
+        self.point = point
+        self.is_play = (self.point is not None)
+        self.is_pass = is_pass
+        self.is_resign = is_resign
+
+    @classmethod
+    def play(cls, point):
+        """Placing a stone on the board"""
+        return Move(point=point)
+
+    @classmethod
+    def pass_turn(cls):
+        """Passing a turn"""
+        return Move(is_pass=True)
+
+    @classmethod
+    def resign(cls):
+        """Exiting the game"""
+        return Move(is_resign=True)
+
+
+class GoString():
+    """A chain of connected stones of the same color."""
+    def __init__(self, color, stones, liberties):
+        self.color = color
+        self.stones = set(stones)
+        self.liberties = set(liberties)
+
+
+    def remove_liberty(self, point):
+        self.liberties.remove(point)
+
+
+    def add_liberty(self, point):
+        self.liberties.add(point)
+
+
+    def merged_with(self, go_string):
+        """Returns a new chain containing all stones from both chains."""
+        assert go_string.color == self.color
+        combined_stones = self.stones | go_string.stones
+        return GoString(
+            self.color,
+            combined_stones,
+            (self.liberties | go_string.liberties) - combined_stones
+        )
+
+
+    @property
+    def num_liberties(self):
+        return len(self.liberties)
+
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, GoString) 
+            and self.color == other.color 
+            and self.stones == other.stones 
+            and self.liberties == other.liberties
+        )
+
+
+class Board():
+    """The board is initialized as an empty grid consisting of a specified number of 
+    rows and columns."""
+    def __init__(self, num_rows, num_cols):
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self._grid = {}
+
+
+    def _remove_string(self, string):
+        for point in string.stones:
+            for neighbor in point.neighbors():
+                # Removing a chain may result in an increase in the liberties of other chains
+                neighbor_string = self._grid.get(neighbor)
+                if neighbor_string is None:
+                    continue
+                if neighbor_string is not string:
+                    neighbor_string.add_liberty(point)
+            self._grid[point] = None
+
+
+    def is_on_grid(self,point):
+        return 1 <= point.row <= self.num_rows and 1 <= point.col <= self.num_cols
+
+
+    def get(self, point):
+        """Returns the content of a point on the board: player information if there is 
+        a stone at that point, otherwise None."""
+        string = self._grid.get(point)
+        if string is None:
+            return None
+        return string.color
+
+
+    def get_go_string(self, point):
+        """Returns the entire chain of stones if there is a stone at this point, 
+        otherwise None."""
+        string = self._grid.get(point)
+        if string is None:
+            return None
+        return string
+
+
+    def place_stone(self, player, point):
+        assert self.is_on_grid(point)
+        assert self._grid.get(point) is None
+        adjacent_same_color = []
+        adjacent_opposite_color = []
+        liberties = []
+        for neighbor in point.neighbors():
+            # The immediate neighbors of a specific point are examined first
+            if not self.is_on_grid(neighbor):
+                continue
+            neighbor_string = self._grid.get(neighbor)
+            if neighbor_string is None:
+                liberties.append(neighbor)
+            elif neighbor_string.color == player:
+                if neighbor_string not in adjacent_same_color:
+                    adjacent_same_color.append(neighbor_string)
+            else:
+                if neighbor_string not in adjacent_opposite_color:
+                    adjacent_opposite_color.append(neighbor_string)
+
+        new_string = GoString(player, [point], liberties)
+
+        # Merging all adjacent chains of stones of the same color
+        for same_color_string in adjacent_same_color:
+            new_string = new_string.merged_with(same_color_string)
+
+        for new_string_point in new_string.stones:
+            self._grid[new_string_point] = new_string
+
+        # Reducing the number of liberties of adjacent chains of stones of the opposite color
+        for other_color_string in adjacent_opposite_color:
+            other_color_string.remove_liberty(point)
+
+        # Removing from the board chains of stones of the opposite color with zero liberties
+        for other_color_string in adjacent_opposite_color:
+            if other_color_string.num_liberties == 0:
+                self._remove_string(other_color_string)
