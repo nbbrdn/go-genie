c32c726ba2f3346c83750f17a86f491e21bf0db6
Nikolay Bayborodin <nbbrdn@gmail.com>
2024-07-30 21:29:53 +1000

    Implement game state

--- goboard_slow.py
+++ goboard_slow.py
@@ -1,11 +1,12 @@
 import copy
 from gotypes import Player
 
-class Move():
+
+class Move:
     def __init__(self, point=None, is_pass=False, is_resign=False):
-        assert(point is not None) ^ is_pass ^ is_resign
+        assert (point is not None) ^ is_pass ^ is_resign
         self.point = point
-        self.is_play = (self.point is not None)
+        self.is_play = self.point is not None
         self.is_pass = is_pass
         self.is_resign = is_resign
 
@@ -25,21 +26,19 @@ class Move():
         return Move(is_resign=True)
 
 
-class GoString():
+class GoString:
     """A chain of connected stones of the same color."""
+
     def __init__(self, color, stones, liberties):
         self.color = color
         self.stones = set(stones)
         self.liberties = set(liberties)
 
-
     def remove_liberty(self, point):
         self.liberties.remove(point)
 
-
     def add_liberty(self, point):
         self.liberties.add(point)
-
 
     def merged_with(self, go_string):
         """Returns a new chain containing all stones from both chains."""
@@ -48,32 +47,30 @@ class GoString():
         return GoString(
             self.color,
             combined_stones,
-            (self.liberties | go_string.liberties) - combined_stones
+            (self.liberties | go_string.liberties) - combined_stones,
         )
-
 
     @property
     def num_liberties(self):
         return len(self.liberties)
 
-
     def __eq__(self, other):
         return (
-            isinstance(other, GoString) 
-            and self.color == other.color 
-            and self.stones == other.stones 
+            isinstance(other, GoString)
+            and self.color == other.color
+            and self.stones == other.stones
             and self.liberties == other.liberties
         )
 
 
-class Board():
-    """The board is initialized as an empty grid consisting of a specified number of 
+class Board:
+    """The board is initialized as an empty grid consisting of a specified number of
     rows and columns."""
+
     def __init__(self, num_rows, num_cols):
         self.num_rows = num_rows
         self.num_cols = num_cols
         self._grid = {}
-
 
     def _remove_string(self, string):
         for point in string.stones:
@@ -86,28 +83,24 @@ class Board():
                     neighbor_string.add_liberty(point)
             self._grid[point] = None
 
-
-    def is_on_grid(self,point):
+    def is_on_grid(self, point):
         return 1 <= point.row <= self.num_rows and 1 <= point.col <= self.num_cols
 
-
     def get(self, point):
-        """Returns the content of a point on the board: player information if there is 
+        """Returns the content of a point on the board: player information if there is
         a stone at that point, otherwise None."""
         string = self._grid.get(point)
         if string is None:
             return None
         return string.color
 
-
     def get_go_string(self, point):
-        """Returns the entire chain of stones if there is a stone at this point, 
+        """Returns the entire chain of stones if there is a stone at this point,
         otherwise None."""
         string = self._grid.get(point)
         if string is None:
             return None
         return string
-
 
     def place_stone(self, player, point):
         assert self.is_on_grid(point)
@@ -146,3 +139,36 @@ class Board():
         for other_color_string in adjacent_opposite_color:
             if other_color_string.num_liberties == 0:
                 self._remove_string(other_color_string)
+
+
+class GameState:
+    def __init__(self, board, next_player, previous, move):
+        self.board = board
+        self.next_player = next_player
+        self.previous_state = previous
+        self.last_move = move
+
+    def apply_move(self, move):
+        if move.is_play:
+            next_board = copy.deepcopy(self.board)
+            next_board.place_stone(self.next_player, move.point)
+        else:
+            next_board = self.board
+        return GameState(next_board, Player.black, None, None)
+
+    def is_over(self):
+        if self.last_move is None:
+            return False
+        if self.last_move.is_resign:
+            return True
+        second_last_move = self.previous_state.last_move
+        if second_last_move is None:
+            return False
+        return self.last_move.is_pass and second_last_move.is_pass
+
+    @classmethod
+    def new_game(cls, board_size):
+        if isinstance(board_size, int):
+            board_size = (board_size, board_size)
+        board = Board(*board_size)
+        return GameState(board, Player.black, None, None)
